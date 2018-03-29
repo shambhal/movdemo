b8a87ac3a8d4c04d040e124d9d272f222bb280e5
shambhal <shambhalnetworks@gmail.com>
2018-03-29 21:33:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ app.set('rts',rts);
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
+    mongoURL = process.env.OPENSHIFT_MODIR_DB_URL || process.env.MONGO_URL,
     mongoURLLabel = "";
 
 if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
